b1643c32557e2a2e24d5b8c0f7b400f0678eccee
Kagiri11 <charlesmaish423@gmail.com>
2021-10-22 19:35:34 +0300

    corrected linting errors

--- data/src/test/java/com/example/data/ExampleUnitTest.kt
+++ data/src/test/java/com/example/data/ExampleUnitTest.kt
@@ -1,7 +1,7 @@
 package com.example.data
 
-import org.junit.Test
 import org.junit.Assert.assertEquals
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
